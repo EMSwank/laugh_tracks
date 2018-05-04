0325d1b7fa4e330410a4341e8d3a3b7753a3c51d
Eliot Swank <eliotswank@gmail.com>
2018-05-04 10:48:47 -0600

    implements specials for comedians

--- spec/features/user_sees_list_of_comedians_spec.rb
+++ spec/features/user_sees_list_of_comedians_spec.rb
@@ -6,6 +6,19 @@ RSpec.describe "user visits comedians" do
     visit('/comedians')
 
     expect(page).to have_content(comedian.name)
-    expect(page).to have_content(comedian.age) 
-  end 
+    expect(page).to have_content(comedian.age)
+  end
+
+  it 'displays a list of comedians and specials' do
+    comedian_name = 'Kathy Griffin'
+    special_name = 'Live in Denver'
+    comedian = Comedian.create(name: comedian_name, age: '55')
+    special = Special.create(name: special_name, comedian_id: 5)
+
+    visit('/comedians')
+
+    expect(page).to have_content(special.name)
+    expect(page).to have_content(special.comedian_id)
+  end
+
 end
